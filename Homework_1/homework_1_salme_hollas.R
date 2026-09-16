e77e2b9146d148055545d019c417d6b45e0e033d
salmehollas <sh5380a@american.edu>
2026-09-15 21:44:15 -0400

    Add assignment screenshots

---
+++ Homework_1/homework_1_salme_hollas.R
@@ -0,0 +1,10 @@
+#Problem 1
+scores <- c(72, 85, 91, 68, 84)
+mean_score <- mean(scores)
+print(mean_score)
+
+#Problem 2
+median_score <- median(scores)
+standard_deviation <- sd(scores)
+print(median_score)
+print(standard_deviation)
